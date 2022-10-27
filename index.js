8b361c929811a3db33df2acc3a6eae7e307f00f5
Shahin Alam <shahinzzz1949@gmail.com>
2022-10-27 09:49:25 +0600

    fetch single and all course data

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ app.use(cors());
 app.get('/',(req,res) => {
     res.send('Server running');
 });
+// get all course data
 app.get('/all-course',(req,res) => {
     res.send(fakeData);
+});
+
+// get single data
+app.get('/all-course/:id',(req,res) => {
+    // console.log(req.params.id);
+    const id = parseInt(req.params.id);
+    const singleData = fakeData.find(course => course.id === id) || [];
+    res.send(singleData);
 });
 
 app.listen(port, ()=>{
